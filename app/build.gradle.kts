799727c5d67f400e019a44b6d5c1e7e19ea24c66
erolgizlice <erolgizlice@gmail.com>
2026-09-18 11:48:06 +0300

    build: set up Hilt in :app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -37,6 +39,12 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+
+    implementation(projects.data)
+    implementation(projects.ui.views)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/erolgizlice/posts/PostsApplication.kt
+++ app/src/main/java/com/erolgizlice/posts/PostsApplication.kt
@@ -1,2 +1,7 @@
 package com.erolgizlice.posts
 
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PostsApplication : Application()
