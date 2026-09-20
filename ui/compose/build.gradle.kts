b5cf92b496dd330042686d43d8a9cbb2174bbdc7
erolgizlice <erolgizlice@gmail.com>
2026-09-20 23:32:09 +0300

    feat(compose): add previews for the posts screen

--- ui/compose/build.gradle.kts
+++ ui/compose/build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.material.icons)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
